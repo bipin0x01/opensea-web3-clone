b215dc7ef0a25dafb753b422db6838854d5cfa90
Bipin Thapa <bt.kaji@gmail.com>
2022-06-09 21:22:16 +0545

    basic frontend design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bipinsea - I see you NFT Marketplace</title>
+        {/* description */}
+        <meta
+          name="description"
+          content="Bipinsea - I see you NFT Marketplace"
+        />
+        {/* keywords */}
+        <meta name="keywords" content="Bipinsea, NFT, Marketplace" />
+        {/* author */}
+        <meta name="author" content="Bipin Thapa" />
+        {/* viewport */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
